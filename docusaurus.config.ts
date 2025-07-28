d1c4b61993446bf3b0ef90bf4e07af824a00204c
BOXmineYT <131708727+BOXmineYT@users.noreply.github.com>
2025-07-29 01:02:33 +0300

    1

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'https://web.box-hosting.ru/icon.png',
 
   // Set the production url of your site here
-  url: 'https://wiki.myid.su',
+  url: 'https://boxmineyt.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
